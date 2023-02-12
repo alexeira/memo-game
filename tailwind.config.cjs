ba26caa9439d8452ac185be8ed406f8bb85fe6f6
alexeira <alex.pincheira06@gmail.com>
2023-02-12 17:49:32 -0300

    add extend styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      borderWidth: {
+        lr: '24px',
+        b: '48px'
+      },
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(100px, 1fr))'
+      }
+    }
   },
   plugins: []
 }
