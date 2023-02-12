87dd2bc339e3c32c1390127dfd6450b24d472343
alexeira <alex.pincheira06@gmail.com>
2023-02-12 15:14:13 -0300

    fix memo component

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -7,15 +7,23 @@ interface Shapes {
   [key: string]: JSX.Element
 }
 
-export default function Memo({ shape, color }: MemoProps) {
+export default function Memo({ color, shape }: MemoProps) {
+  const styles = {
+    square: `w-6 h-6`,
+    circle: `w-6 h-6 rounded-full`,
+    triangle: `border-l-[12px] border-r-[12px] border-b-[23px] border-l-transparent border-r-transparent`
+  }
+
+  const colors = {
+    square: color,
+    circle: color,
+    triangle: color
+  }
+
   const shapes: Shapes = {
-    square: <div className={`w-6 h-6 bg-${color}-500`} />,
-    circle: <div className={`w-6 h-6 rounded-full bg-${color}-500`} />,
-    triangle: (
-      <div
-        className={`border-l-[12px] border-r-[12px] border-b-[23px] border-l-transparent border-r-transparent bg-${color}-500`}
-      />
-    )
+    square: <div className={styles.square} style={{ backgroundColor: colors.square }} />,
+    circle: <div className={styles.circle} style={{ backgroundColor: colors.circle }} />,
+    triangle: <div className={styles.triangle} style={{ borderBottomColor: colors.triangle }} />
   }
 
   return shapes[shape]
