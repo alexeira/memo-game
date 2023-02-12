4dce7e1568394d0ebaed022af3b860453236ce98
alexeira <alex.pincheira06@gmail.com>
2023-02-12 14:25:35 -0300

    minimal changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Memo from './components/Memo'
 import Wrapper from './components/Wrapper'
 
 export default function App() {
   return (
     <Wrapper>
-      <h1>Hello</h1>
+      <Memo color="red" shape="square" />
+      <Memo color="blue" shape="circle" />
+      <Memo color="yellow" shape="triangle" />
     </Wrapper>
   )
 }
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,3 +1,7 @@
 export default function Wrapper({ children }: any) {
-  return <main className="min-h-screen bg-slate-700 m-auto">{children}</main>
+  return (
+    <main className="min-h-screen grid place-items-center bg-slate-800">
+      <div className="max-w-3xl border border-white">{children}</div>
+    </main>
+  )
 }
