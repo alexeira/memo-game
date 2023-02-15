019dbb31f29c726455790f5bbaaa6781d2c5e66c
alexeira <alex.pincheira06@gmail.com>
2023-02-15 15:40:52 -0300

    changed max-width

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,7 @@
 export default function Wrapper({ children }: any) {
   return (
     <main className="min-h-screen grid place-items-center bg-slate-800">
-      <div className="max-w-2xl w-full border border-gray-500 rounded-sm">{children}</div>
+      <div className="max-w-4xl w-full border p-3 border-gray-500 rounded-sm">{children}</div>
     </main>
   )
 }
