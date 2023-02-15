9ff756bd10f7d6c9488d824784ac6468b23bc9f4
alexeira <alex.pincheira06@gmail.com>
2023-02-15 15:43:44 -0300

    add pentagon to shapes

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -9,21 +9,24 @@ interface Shapes {
 
 export default function Figure({ color, shape }: MemoProps) {
   const styles = {
-    square: `w-12 h-12`,
-    circle: `w-12 h-12 rounded-full`,
-    triangle: `w-12 border-x-lr border-b-b border-x-transparent`
+    square: 'w-12 h-12',
+    circle: 'w-12 h-12 rounded-full',
+    triangle: 'border-x-lr border-b-b border-x-transparent',
+    pentagon: 'w-12 h-12 clip-polygon'
   }
 
   const colors = {
     square: color,
     circle: color,
-    triangle: color
+    triangle: color,
+    pentagon: color
   }
 
   const shapes: Shapes = {
     square: <div className={styles.square} style={{ backgroundColor: colors.square }} />,
     circle: <div className={styles.circle} style={{ backgroundColor: colors.circle }} />,
-    triangle: <div className={styles.triangle} style={{ borderBottomColor: colors.triangle }} />
+    triangle: <div className={styles.triangle} style={{ borderBottomColor: colors.triangle }} />,
+    pentagon: <div className={styles.pentagon} style={{ backgroundColor: colors.pentagon }} />
   }
 
   return shapes[shape]
